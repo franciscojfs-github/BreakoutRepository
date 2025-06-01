c5b3f890f93b21146f6faafabc2fd44b83a1f60b
franciscojfs-github <franciscojfs.work@gmail.com>
2025-05-31 22:47:03 -0400

    Tarea Practica-Modulo12
    Se subieron los cambios para entregar la tarea. Se creo la escena con los bloques, bola y jugador. La bola cae sobre el jugador y sale disparada al presionar espacio o boton 0 de xbox control, pero no hace nada mas por ahora

--- Breakout/Assets/_Scripts/Bola.cs
+++ Breakout/Assets/_Scripts/Bola.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Bola : MonoBehaviour
 {
+    bool isGameStarted = false;
+    [SerializeField]public float velocidadBola = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 posicionInicial = GameObject.FindGameObjectWithTag("Jugador").transform.position;
+        posicionInicial.y += 3;
+        this.transform.position = posicionInicial;
+        this.transform.SetParent(GameObject.FindGameObjectWithTag("Jugador").transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) || Input.GetButton("Submit"))
+        {
+            if(!isGameStarted)
+            {
+                isGameStarted = true;
+                this.transform.SetParent(null);
+                GetComponent<Rigidbody>().velocity = velocidadBola * Vector3.up;
+            }
+        }
     }
 }
--- Breakout/Assets/_Scripts/Jugador.cs
+++ Breakout/Assets/_Scripts/Jugador.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
+    [SerializeField] public int limiteX = 24;
+    [SerializeField] public float velocidadPaddle = 30f;
+    
+    Transform transform;
+    Vector3 mousePos2D;
+    Vector3 mousePos3D;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform = this.gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //Vector3 pos = this.transform.position;
+        //Vector3 posMouse = transform.position;        //linea de codigo irrelevante, se trato de agregar con el else para tratar de que funcionara con teclas y mouse al mismo tiempo
+
+        //if (Input.GetKey(KeyCode.RightArrow))                                             //este codigo es para que se puedan usar las teclas, se comenta para usar control
+        //{
+        //    transform.Translate(Vector3.down * velocidadPaddle * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    transform.Translate(Vector3.up * velocidadPaddle * Time.deltaTime);
+        //}
+        ////////////else
+        ////////////{
+        ////////////    posMouse.x = mousePos3D.x;        //se trato de que funcionara con ambos mouse  y teclas pero no se puede asi
+        ////////////}
+
+        transform.Translate(Input.GetAxis("Horizontal") * Vector3.down * velocidadPaddle * Time.deltaTime);     //Para control gamer. deltaTime es la diferencia de tiempo que hay entre frames
+        //GetAxis funciona con cualquier control, joystick o teclado. "down" porque esta rotado 90 grados. (y -1?)
+
+        Vector3 pos = transform.position;
+        //pos.x = mousePos3D.x;                 //activar esta linea para jugar con mouse
+        if (pos.x < -limiteX)
+        {
+            pos.x = -limiteX;
+        }
+        else if (pos.x > limiteX)
+        {
+            pos.x = limiteX;
+        }
+        transform.position = pos;
+
+        ////////////Vector3 posMouse = transform.position;  //se trato de hacer que funcionara las teclas y mouse al mismo tiempo pero no se logro
+        ////////////posMouse.x = mousePos3D.x;
+        ////////////if (posMouse.x < -limiteX)
+        ////////////{
+        ////////////    posMouse.x = -limiteX;
+        ////////////}
+        ////////////else if (posMouse.x > limiteX)
+        ////////////{
+        ////////////    posMouse.x = limiteX;
+        ////////////}
+        ////////////transform.position = posMouse;        //como al final queda guardada la posicion del mouse para mover al jugador, ya no le hace caso al transfor.position de las teclas antes de frame update
     }
 }
